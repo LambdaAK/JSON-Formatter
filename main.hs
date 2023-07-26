cd39da2b7a7bf5e97f3b1d7abef01954de107beb
Alex Kozik <ajk333@cornell.edu>
2023-07-25 22:26:59 -0400

    Added lexing for numbers

--- main.hs
+++ main.hs
@@ -1,3 +1,6 @@
+import Debug.Trace
+import Data.Function
+
 data Token =
     LBRACE
     | RBRACE
@@ -10,10 +13,37 @@ data Token =
     | TRUE
     | FALSE
     | NULL
+    | NUMBER Double
     deriving (Show)
 
 isLetter :: Char -> Bool
 isLetter c = c `elem` ['a'..'z'] || c `elem` ['A'..'Z']
+
+isDigit :: Char -> Bool
+isDigit c = c `elem` ['0'..'9']
+
+lexNumber :: String -> Maybe (Double, String)
+lexNumber tokens = do
+    (lexedDigits, remainder) <- lexDigits tokens False
+    let number = read lexedDigits :: Double
+    return (number, remainder)
+
+lexDigits :: String -> Bool -> Maybe (String, String)
+-- string is the tokens
+-- is weather or not we have seen a decimal point
+
+lexDigits [] _ = Just ("", [])
+lexDigits (x:xs) seenDecimal
+  | x == '.' = if seenDecimal
+                then Nothing
+                else do
+                    (lexedDigits, remainder) <- lexDigits xs True
+                    return (x:lexedDigits, remainder)
+  | isDigit x = do
+            (lexedDigits, remainder) <- lexDigits xs seenDecimal
+            return (x:lexedDigits, remainder)
+  | otherwise = return ("", x:xs)
+
 
 lexString :: String -> Maybe (String, String)
 -- first string is the lexed string, second string is the remainder of the input
@@ -24,7 +54,6 @@ lexString (x : xs)
   | otherwise = do
         (lexedString :: String, remainder :: String) <- lexString xs
         return (x:lexedString, remainder)
-
 
 lexTokens :: String -> Maybe [Token]
 lexTokens [] = Just []
@@ -47,6 +76,12 @@ lexTokens s =
                     let newToken :: Token = STRING lexedString in do
                         remainderTokens :: [Token] <- lexTokens remainder
                         return (newToken:remainderTokens)
+            else if isDigit x
+                then do
+                    (number :: Double, remainder :: String) <- lexNumber (x:xs)
+                    let newToken :: Token = NUMBER number in do
+                        remainderTokens :: [Token] <- lexTokens remainder
+                        return (newToken:remainderTokens)
                 else
                     let newToken :: Token = case x of
                             '{' -> LBRACE
@@ -65,5 +100,4 @@ lexTokens s =
 
 main :: IO ()
 main = do
-    let a = lexTokens "{\"aa\"}:::::::::null:,,,,,,}}}}{{{{{{true      false}}}}}}"
-    print a
+    "{12.1.}" & lexTokens & print
